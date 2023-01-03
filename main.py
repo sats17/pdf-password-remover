979a5a501e6aa2565083c9fcddae7ccb9c10c4a1
sats17 <satishkumbhar9999@gmail.com>
2023-01-03 20:26:16 +0530

    Add password here

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-# import PdfFileWriter and PdfFileReader
-# class from PyPDF2 library
 from PyPDF2 import PdfWriter, PdfReader
 
 # Create a PdfFileWriter object
 out = PdfWriter()
 
 # Open encrypted PDF file with the PdfFileReader
-file = PdfReader("encrypted.pdf")
+file_name = "encrypted.pdf"
+file = PdfReader(file_name)
 
 # Store correct password in a variable password.
-password = "GBEPK0250B"
+password = "Update password here"
 
 # Check if the opened file is actually Encrypted
 if file.is_encrypted:
@@ -17,26 +16,17 @@ if file.is_encrypted:
     # If encrypted, decrypt it with the password
     file.decrypt(password)
 
-    # Now, the file has been unlocked.
-    # Iterate through every page of the file
-    # and add it to our new file.
-    for idx in range(len(file.pages)):
+    for page_no in range(len(file.pages)):
         # Get the page at index idx
-        page = file.pages[idx]
+        page = file.pages[page_no]
 
         # Add it to the output file
         out.add_page(page)
 
-    # Open a new file "myfile_decrypted.pdf"
-    with open("myfile_decrypted.pdf", "wb") as f:
-
-        # Write our decrypted PDF to this file
+    decrypted_file_name = "decrypted_"+file_name
+    with open(decrypted_file_name, "wb") as f:
         out.write(f)
 
-    # Print success message when Done
-    print("File decrypted Successfully.")
+    print("File decrypted Successfully, please check file "+decrypted_file_name)
 else:
-
-    # If file is not encrypted, print the
-    # message
-    print("File already decrypted.")
+    print("File already decrypted, no need to decrypt it")
